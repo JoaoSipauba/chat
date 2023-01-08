364adf1fe29ff57b1336e10b0c15aa7072e6861f
joao.sipauba <joao.sipauba@ceuma.br>
2023-01-08 20:11:43 -0300

    feat: enviando mensagens

--- chat-client/src/pages/room/index.tsx
+++ chat-client/src/pages/room/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom'
-import { io } from "socket.io-client";
+import { sendMessage, socket } from '../../service/socket';
 
 import './index.css'
 
@@ -11,28 +11,39 @@ interface IMessage {
   text: string
 }
 
-const socket = io('http://localhost:3333');
-
 function RoomPage() {
   const navigate = useNavigate();
   const query = useQuery();
+  
   const [messages, setMessages] = useState<IMessage[]>([]);
-
+  const [text, setText] = useState('')
+  
   useEffect(() => {    
-    socket.on('message', message => {      
-      setMessages([...messages, message]);      
+    socket.on('message', message => {  
+      message.created_at = new Date(message.created_at);    
+      setMessages([...messages, message]);  
+      setText('')    
     })
-
-  }, [])
+  }, [messages])
 
   function useQuery() {
     const { search } = useLocation();
-  
+
     return useMemo(() => new URLSearchParams(search), [search]);
   }
 
   function logout() {
     navigate('/login')
+  }
+
+  function handleKeyPress(event: any){
+    if(event.key === 'Enter'){
+      sendMessage({
+        message: text,
+        username: query.get('username'),
+        room: query.get('room')
+      })
+    }
   }
 
   return (
@@ -42,12 +53,12 @@ function RoomPage() {
         <div className="chat">
             <p id='welcome'>Olá <strong>{query.get('username')}</strong>. Seja bem vindo à sala <strong>{query.get('room')}</strong>.</p>
             <div id="text">
-              {messages.map((message, index) => <p key={index}><strong>{message.username}</strong> {message.text} - 10/10 10:20</p>)}
+              {messages.map((message, index) => <p key={index}><strong>{message.username}</strong> {message.text} - {`${message.created_at.getHours()}:${message.created_at.getMinutes()}`}</p>)}
             </div>
         </div>
     </header>
     <section className="box-search">
-        <input className="search" type="text" name="Nome" placeholder="Digite sua mensagem" />
+        <input className="search" type="text" name="Nome" placeholder="Digite sua mensagem" onKeyPress={handleKeyPress} onChange={e => setText(e.target.value)} value={text}/>
     </section>
     </>
   )
--- chat-client/src/service/socket.ts
+++ chat-client/src/service/socket.ts
@@ -1,15 +1,28 @@
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:3333');
+export const socket = io('http://localhost:3333');
 
 interface ISelectRoom{
     username: string
     room: string
+}
+interface IMessage{
+    username: string | null
+    room: string | null
+    message: string
 }
 
 export function selectRoom(user: ISelectRoom) {
     socket.emit('select_room', {
         username: user.username, 
         room: user.room
+    })
+}
+
+export function sendMessage(message: IMessage) {
+    socket.emit('message', {
+        username: message.username, 
+        room: message.room,
+        message: message.message
     })
 }
